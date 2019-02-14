431dc5ae93f45e4c834d280b0a47481405ed4f6b
Leszek Brzoska <super.programisci@gmail.com>
2019-02-14 10:26:30 +0100

    komentarz

--- src/start/Hello.java
+++ src/start/Hello.java
@@ -2,6 +2,8 @@ package start;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.printf("Hello World");
+        System.out.printf("Hello World! ");
+        System.out.println("JRE version: " + System.getProperty("java.runtime.version"))
+                ;
     }
 }
